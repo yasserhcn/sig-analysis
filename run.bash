1fc84400ab5f0078aa1e47976e99fc4b01fe0646
yasserhcn <hacini_yasser@yahoo.fr>
2022-12-27 16:28:37 +0100

    changed build files for easier debugging

--- run.bash
+++ run.bash
@@ -3,5 +3,16 @@ g++ src/main.cpp src/files/wav/openWav.cpp src/display/window.cpp \
     src/display/tab.cpp src/imgui/misc/cpp/imgui_stdlib.cpp src/display/data.cpp src/dsp/fft.cpp \
     -I"./src" -I"./src/include" \
     -L"./build" -lsfml-graphics -lsfml-window -lsfml-system -lImGui-SFML -lpthread\
-    -o build/sig.out -std=c++2a
+    -o build/sig -std=c++2a
 echo "done building."
+
+echo "setting up xwindow"
+cd build
+export DISPLAY=$(route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0
+
+echo "running app"
+./sig
+
+echo "app closed"
+cd ..
+rm ./build/sig
